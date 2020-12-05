6184d1074db7bf5f1268778f52e307f05aa17010
Oliver Frank <oliverfrank321@gmail.com>
2020-12-05 13:40:04 +0000

    Finish day 5

--- prob5_1/src/main.rs
+++ prob5_1/src/main.rs
@@ -1,3 +1,59 @@
+extern crate aoc;
+
 fn main() {
-    println!("Hello, world!");
+    let input = aoc::input_to_str("5");
+
+    let mut largest = 0;
+    for line in input.lines() {
+
+        // DEBUG
+        // println!("Row: {} \tCol:{} \tID: {}", get_row(line), get_col(line), get_id(line));
+
+        let id = get_id(line);
+        if id > largest {
+            largest = id;
+        }
+    }
+
+    println!("Result: {}", largest);
+}
+
+fn get_row(pass: &str) -> i32 {
+    let row_code = &pass[0..7];
+    let mut lower = 0;
+    let mut upper = 127;
+
+    for c in row_code.chars() {
+        let shift = ((upper - lower) / 2) + 1;
+        if c == 'F' {
+            upper = upper - shift;
+        }
+        else {
+            lower = lower + shift;
+        }
+    }
+
+    upper
+}
+
+fn get_col(pass: &str) -> i32 {
+    let col_code = &pass[7..];
+    let mut lower = 0;
+    let mut upper = 7;
+
+    for c in col_code.chars() {
+        let shift = ((upper - lower) / 2) + 1;
+        if c == 'L' {
+            upper = upper - shift;
+        }
+        else {
+            lower = lower + shift;
+        }
+    }
+
+    upper
+}
+
+fn get_id(pass: &str) -> i32 {
+    (get_row(pass) * 8) + get_col(pass)
 }
--- prob5_2/src/main.rs
+++ prob5_2/src/main.rs
@@ -1,3 +1,74 @@
+extern crate aoc;
+
 fn main() {
-    println!("Hello, world!");
+    let input = aoc::input_to_str("5");
+
+    let mut id_list: Vec<i32> = vec![];
+    for line in input.lines() {
+        let mut inserted = false;
+        let current_id = get_id(line);
+        for (i, &id) in (&id_list).into_iter().enumerate() {
+            if current_id < id {
+                id_list.insert(i, current_id);
+                inserted = true;
+                break;
+            }
+        }
+        if !inserted {
+            id_list.push(current_id);
+        }
+    }
+
+    let mut iter = id_list.into_iter();
+    let mut prev = iter.next().expect("Empty list");
+    let mut res_id = 0;
+    for id in iter {
+        if id - 1 != prev {
+            res_id = id - 1;
+            break;
+        }
+        prev = id;
+    }
+
+    println!("Result {}", res_id);
+}
+
+fn get_row(pass: &str) -> i32 {
+    let row_code = &pass[0..7];
+    let mut lower = 0;
+    let mut upper = 127;
+
+    for c in row_code.chars() {
+        let shift = ((upper - lower) / 2) + 1;
+        if c == 'F' {
+            upper = upper - shift;
+        }
+        else {
+            lower = lower + shift;
+        }
+    }
+
+    upper
+}
+
+fn get_col(pass: &str) -> i32 {
+    let col_code = &pass[7..];
+    let mut lower = 0;
+    let mut upper = 7;
+
+    for c in col_code.chars() {
+        let shift = ((upper - lower) / 2) + 1;
+        if c == 'L' {
+            upper = upper - shift;
+        }
+        else {
+            lower = lower + shift;
+        }
+    }
+
+    upper
+}
+
+fn get_id(pass: &str) -> i32 {
+    (get_row(pass) * 8) + get_col(pass)
 }
